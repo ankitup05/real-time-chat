03fc6e03e5a289623785fcda931193518a512dd9
ankitup05 <ankitup05@gmail.com>
2023-06-21 22:52:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,6 @@ res.send(singleChat);
 // console.log(singleChat);
 });
 
-const PORT=process.env.PORT || 5000
+const PORT=process.env.PORT || 9000
 
 app.listen(PORT,console.log(`Server started on port ${PORT}` )); 
